e3ca18cb57021cac0c6dd953ec726d7d4f06402b
Luis Barreto <luis.barreto.marin@gmail.com>
2023-05-06 11:36:14 -0400

    Add: Startup file

---
+++ UcabGo.Api/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(UcabGo.Api.Startup))]
+
+namespace UcabGo.Api
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+
+            //builder.Services.Add(
+            //    new ServiceDescriptor(
+            //        typeof(IBaseDatabaseService), 
+            //        (serviceProvider) => new CentralDatabaseService(connectionString),
+            //        ServiceLifetime.Scoped
+            //));
+
+            ////Dependency injection
+            //builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            //builder.Services.AddTransient<ICallDetailService, CallDetailService>();
+        }
+    }
+}
