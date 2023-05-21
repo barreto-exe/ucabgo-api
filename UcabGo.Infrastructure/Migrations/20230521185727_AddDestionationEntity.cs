4ba1ef6a9d342da98e90c70af38fcc87fea9ac5b
Luis Barreto <luis.barreto.marin@gmail.com>
2023-05-21 14:58:20 -0400

    Add: Migration for Add: Destination

---
+++ UcabGo.Infrastructure/Migrations/20230521185727_AddDestionationEntity.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UcabGo.Infrastructure.Migrations
+{
+    public partial class AddDestionationEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "destinations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int(11)", nullable: false, comment: " ")
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    User = table.Column<int>(type: "int(11)", nullable: false),
+                    Alias = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Zone = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Detail = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Latitude = table.Column<float>(type: "float", nullable: false),
+                    Longitude = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_destinations", x => x.Id);
+                    table.ForeignKey(
+                        name: "destinations_ibfk_1",
+                        column: x => x.User,
+                        principalTable: "user",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "User",
+                table: "destinations",
+                column: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "destinations");
+        }
+    }
+}
