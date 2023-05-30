fe89bc7868f8b33d569fd46d601a867753fe2693
Luis Barreto <luis.barreto.marin@gmail.com>
2023-05-30 18:20:28 -0400

    Add: Accept only UCAB email adresses

--- UcabGo.Core/Data/Auth/Inputs/RegisterInput.cs
+++ UcabGo.Core/Data/Auth/Inputs/RegisterInput.cs
@@ -5,10 +5,15 @@ namespace UcabGo.Core.Data.Auth.Inputs
     public class RegisterInput : BaseRequest
     {
         [Required]
+        [RegularExpression(
+            @"^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)?ucab\.edu\.ve$", 
+            ErrorMessage = "Invalid email address. Only UCAB email addresses are accepted")]
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must: (1) Be at least 8 characters long. (2) Contain at least one capital letter. (3) Contain at least one number. (4) Contain at least one special character.")]
+        [RegularExpression(
+            @"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", 
+            ErrorMessage = "Password must: (1) Be at least 8 characters long. (2) Contain at least one capital letter. (3) Contain at least one number. (4) Contain at least one special character.")]
         public string Password { get; set; }
 
         [Required]
