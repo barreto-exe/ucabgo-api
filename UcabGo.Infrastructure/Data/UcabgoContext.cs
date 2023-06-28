73d24a9cda489044f350d8777a28819b3e428895
Luis Barreto <luis.barreto.marin@gmail.com>
2023-06-28 01:16:46 -0400

    Add: Migration for merging Locations and Destinations

--- UcabGo.Infrastructure/Data/UcabgoContext.cs
+++ UcabGo.Infrastructure/Data/UcabgoContext.cs
@@ -27,8 +27,8 @@ namespace UcabGo.Infrastructure.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost,3306;user=root;password=labm-801;database=ucabgo1", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.10.2-mariadb"));
+                string connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
             }
         }
 
